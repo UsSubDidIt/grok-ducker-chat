5ef5789a1953cc398b4f9aeb0b59c66eb10315b2
Tethys Neuroplexus <isneuroplexus@duck.com>
2025-04-10 17:04:33 +1000

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   // Removing 'output: export' to support dynamic routes
   // output: 'export',
-  distDir: './dist', // Changes the build output directory to match Vite's
   reactStrictMode: true,
 }
 
